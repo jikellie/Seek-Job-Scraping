faa46485daf9b9cc0937ccfd497daf3c35143e1c
jikellie <100405863+jikellie@users.noreply.github.com>
2023-04-11 18:31:49 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from POL import Image
+
+st.set_page_config(page_tite= 'Survey Results')
+st.header('Survey Results 2021')
+st.subheader('Was the tutorial helpful')
