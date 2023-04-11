e84cfc26c21fa66cea5efc914d4745c04e3eaac6
jikellie <100405863+jikellie@users.noreply.github.com>
2023-04-11 19:38:15 +0900

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import streamlit as st
-import plotly.express as px
-from POL import Image
-
-st.set_page_config(page_tite= 'Survey Results')
-st.header('Survey Results 2021')
-st.subheader('Was the tutorial helpful')
